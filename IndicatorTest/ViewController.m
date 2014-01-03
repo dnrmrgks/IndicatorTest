0dcc02c90b729d0f9271e01f54e4bc8abbedc683
park sung chan <dnrmrgks@naver.com>
2014-01-03 15:45:34 +0900

    인디케이터
    인디케이터실

--- IndicatorTest/ViewController.m
+++ IndicatorTest/ViewController.m
@@ -9,15 +9,39 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator1;
+
+@property (strong, nonatomic)UIActivityIndicatorView *indicator2;
 
 @end
 
 @implementation ViewController
 
+
+- (IBAction)startIndicator:(id)sender {
+    [_indicator1 startAnimating];
+    
+    [_indicator2 performSelector:@selector(startAnimating) withObject:nil afterDelay:0.3];
+}
+
+- (IBAction)stopIndicator:(id)sender {
+    [_indicator1 stopAnimating];
+    
+    [_indicator2 performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.7];
+    
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    _indicator2 = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    _indicator2.hidesWhenStopped = YES;
+    _indicator2.center =CGPointMake(160, 200);
+    [self.view addSubview:_indicator2];
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
